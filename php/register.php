3fbd5ae1f01a90ec4ef82ea6eaa78de0e39a078f
Siddharth S <81311756+siddharth-spyder@users.noreply.github.com>
2021-04-27 20:00:45 +0530

    Create register.php
    Sample code for registration in php

---
+++ php/register.php
@@ -0,0 +1,28 @@
+<?php
+require"config.php";
+if(isset($_POST['submit']))
+    {
+        $name=$_POST['name'];
+        $fullname=$_POST['fullname'];
+        $password=$_POST['pass'];
+	
+        if($name=="" && $fullname=="" && $password=="")
+		{
+            echo"please fill in all the information";
+        }
+        else
+        {
+            $j_qry=$conn->query("Insert into databasename(username,fullname,password) values('$name','$fullname','$password')");
+            if($j_qry)
+            {
+                echo"registered successfully";
+echo"<script>location.href='youractionfilename.html/php'</script>";
+            }
+            else{
+                echo"invalid";
+                
+            }
+        }
+
+    }
+?>
